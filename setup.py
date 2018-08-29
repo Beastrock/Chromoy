9e259df00d45b37c1cc8c0ecb453f3a006b826a8
savchenko <savchenko.aleksey@zoloto585.ru>
2018-08-29 14:01:51 +0300

    Change version in setup file.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author_email="beastrock@mail.ru",
     url="https://github.com/Beastrock/Chromoy",
     description="Refreshed chrome-driver for parsing and cron-testing.",
-    version='0.04',  # Update the version number for new releases,
+    version='0.05',  # Update the version number for new releases,
     packages=['chromoy'],
     install_requires=[
         'selenium==3.14.0',
